24b47657027290137051ecc06fb8324a9f5e3ab3
leeseoeun <seoeun.shuen@gmail.com>
2022-01-30 21:33:01 +0900

    0130 직사각형 별찍기

---
+++ app/src/main/java/programmers/level1/직사각형_별찍기.java
@@ -0,0 +1,19 @@
+package programmers.level1;
+
+import java.util.Scanner;
+
+public class 직사각형_별찍기 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    for (int j = 0; j < b; j++) {
+      for (int i = 0; i < a; i++) {
+        System.out.print("*");
+      }
+
+      System.out.println();
+    }
+  }
+}
