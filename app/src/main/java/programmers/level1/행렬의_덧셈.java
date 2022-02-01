bf445bd6fc916f4c699345c9fd8c94d2162ff6e6
leeseoeun <seoeun.shuen@gmail.com>
2022-02-01 22:52:31 +0900

    0201 행렬의 덧셈

---
+++ app/src/main/java/programmers/level1/행렬의_덧셈.java
@@ -0,0 +1,19 @@
+package programmers.level1;
+
+public class 행렬의_덧셈 {
+  public int[][] solution(int[][] arr1, int[][] arr2) {
+    int[][] answer = new int[arr1.length][arr1[0].length];  //'열'은 첫번째 배열의 길이
+
+    for (int i = 0; i < arr1.length ; i++) {
+      for (int j = 0; j < arr1[i].length; j++) {
+        answer[i][j] = arr1[i][j] + arr2[i][j];
+      }
+    }
+
+    return answer;
+  }
+}
+
+//2차원 배열
+//int[][] array4 = new int[3][4];
+//3개의 배열, 정수를 4개씩 담을 수 있는
