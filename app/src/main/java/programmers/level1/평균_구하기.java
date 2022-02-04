dbb9234985a2de8e0a0bdda4af437f8b1ba86f42
leeseoeun <seoeun.shuen@gmail.com>
2022-02-04 18:15:35 +0900

    0204 평균 구하기 및 Update README.md

---
+++ app/src/main/java/programmers/level1/평균_구하기.java
@@ -0,0 +1,17 @@
+package programmers.level1;
+
+public class 평균_구하기 {
+  public double solution(int[] arr) {
+    double answer = 0;
+
+    double total = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      total += arr[i];
+    }
+
+    answer = total / arr.length;
+
+    return answer;
+  }
+}
