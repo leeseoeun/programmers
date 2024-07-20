f7824dff94cb9eb2d74d5b21ada21e9626a0f8c9
leeseoeun <seoeun.shuen@gmail.com>
2024-07-20 19:45:31 +0900

    0720 입문 Day 1 사칙연산

---
+++ app/src/main/java/programmers/beginner/Day1_사칙연산_1_두_수의_합.java
@@ -0,0 +1,21 @@
+package programmers.beginner;
+
+import java.util.Scanner;
+
+public class Day1_사칙연산_1_두_수의_합 {
+
+  public static int solution(int num1, int num2) {
+    return num1 + num2;
+  }
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+
+    System.out.println(solution(num1, num2));
+
+  }
+
+}
