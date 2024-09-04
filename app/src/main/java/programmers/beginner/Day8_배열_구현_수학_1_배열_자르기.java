a6c92c4d8e3549e10ca665b62bcfe418650b3bef
leeseoeun <seoeun.shuen@gmail.com>
2024-09-05 02:29:41 +0900

    0905 입문 Day 8 배열, 구현, 수학

--- app/src/main/java/programmers/beginner/Day8_배열_구현_수학_1_배열_자르기.java
+++ app/src/main/java/programmers/beginner/Day8_배열_구현_수학_1_배열_자르기.java
@@ -2,8 +2,35 @@ package programmers.beginner;
 
 public class Day8_배열_구현_수학_1_배열_자르기 {
 
+  public static int[] solution(int[] numbers, int num1, int num2) {
+    int len = num2 - num1 + 1;
+    int[] answer= new int[len];
+
+    for (int i = 0; i < len; i++) {
+      answer[i] = numbers[num1 + i];
+    }
+
+    //    int[] answer = new int[num2 - num1 + 1];
+    //
+    //    int idx = 0;
+    //    for (int i = num1; i <= num2; i++) {
+    //      answer[idx++] = numbers[i];
+    //    }
+
+    return answer;
+
+    /*
+     * return Arrays.copyOfRange(numbers, num1, num2 + 1);
+     * */
+  }
+
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+
+    int[] numbers = {1, 2, 3, 4, 5};
+    int num1 = 1;
+    int num2 = 3;
+
+    System.out.println(solution(numbers, num1, num2));
 
   }
 
